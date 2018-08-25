c1d67de75d8c49290358626f2e7575c7eceb9b67
Debalina <mukherjee.debalina@gmail.com>
2018-08-25 14:20:21 -0700

    (feat) update search method to cache response data to redis

--- src/server/search.js
+++ src/server/search.js
@@ -15,16 +15,16 @@ const search = asyncMiddleware(async (req, res) => {
   const term = req.params;
   console.log(term.location);
   let formattedData = [];
-  let results = await redis.getSearchResults(term.location);
+  let results;
+  results = await redis.getSearchResults(term.location);
   if (results) {
-    console.log('inside loop~~~~~~');
     formattedData = { timeTaken: 5, count: results.length, data: results };
   } else {
-    console.log('~~~~~~~~~~~inside else~~~~~~');
     results = await searchQuery.queryTerm(term);
     formattedData = formatData(results);
   }
   res.status(200).send(formattedData);
+  redis.writeSearchToCache(term.location, formattedData.data);
 });
 
 module.exports = {
